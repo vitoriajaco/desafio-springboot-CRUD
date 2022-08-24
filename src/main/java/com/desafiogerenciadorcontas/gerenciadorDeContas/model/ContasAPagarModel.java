7194bbe295b726f205ff4fb2c136934a9c6a7c47
vitoriajaco <vitoriajaco@gmail.com>
2022-08-24 16:41:26 -0300

    Implementando classe Model

--- src/main/java/com/desafiogerenciadorcontas/gerenciadorDeContas/model/ContasAPagarModel.java
+++ src/main/java/com/desafiogerenciadorcontas/gerenciadorDeContas/model/ContasAPagarModel.java
@@ -1,11 +1,33 @@
 package com.desafiogerenciadorcontas.gerenciadorDeContas.model;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
+@Getter
+@Setter
+@Entity
+@Table(name = "gerenciadorContas")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContasAPagarModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String status;
+    @Column
     private String nome;
+    @Column
     private Double valor;
+    @Column
+    private String tipo;
+    @Column
     private LocalDate dataDeVencimento;
+    @Column
     private LocalDateTime dataDePagamento;
 }
